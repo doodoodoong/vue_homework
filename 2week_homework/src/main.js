ecd8d2c71c016466ff9677872258f6872afe79a0
doodoodoong <doo_doong@hanmail.net>
2022-11-08 00:22:48 +0900

    #

--- 2week_homework/src/main.js
+++ 2week_homework/src/main.js
@@ -9,3 +9,28 @@ item[0].addEventListener("click", function () {
   img_item[0].style.height = "100%";
   img_item[0].style.transitionDuration = "1s";
 });
+item[1].addEventListener("click", function () {
+  img_item[1].style.width = "100%";
+  img_item[1].style.height = "100%";
+  img_item[1].style.transitionDuration = "1s";
+});
+item[2].addEventListener("click", function () {
+  img_item[2].style.width = "100%";
+  img_item[2].style.height = "100%";
+  img_item[2].style.transitionDuration = "1s";
+});
+item[3].addEventListener("click", function () {
+  img_item[3].style.width = "100%";
+  img_item[3].style.height = "100%";
+  img_item[3].style.transitionDuration = "1s";
+});
+item[4].addEventListener("click", function () {
+  img_item[4].style.width = "100%";
+  img_item[4].style.height = "100%";
+  img_item[4].style.transitionDuration = "1s";
+});
+item[5].addEventListener("click", function () {
+  img_item[5].style.width = "100%";
+  img_item[5].style.height = "100%";
+  img_item[5].style.transitionDuration = "1s";
+});
