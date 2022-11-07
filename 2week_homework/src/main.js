99e6675d6ccbc3e40c722d9039ffdd3b9d90ee7d
doodoodoong <doo_doong@hanmail.net>
2022-11-08 00:07:39 +0900

    #

--- 2week_homework/src/main.js
+++ 2week_homework/src/main.js
@@ -7,4 +7,5 @@ let img_item = document.querySelectorAll("img");
 item[0].addEventListener("click", function () {
   img_item[0].style.width = "100%";
   img_item[0].style.height = "100%";
+  img_item[0].style.transitionDuration = "1s";
 });
