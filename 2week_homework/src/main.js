88119120332dfdf556b762dbd9ec1a08a075e0e4
doodoodoong <doo_doong@hanmail.net>
2022-11-07 14:47:39 +0900

    #

--- 2week_homework/src/main.js
+++ 2week_homework/src/main.js
@@ -1,1 +1,10 @@
 ScrollOut();
+
+let item = document.querySelectorAll(".board_item");
+
+let img_item = document.querySelectorAll("img");
+
+item[0].addEventListener("click", function () {
+  img_item[0].style.width = "100%";
+  img_item[0].style.height = "100%";
+});
