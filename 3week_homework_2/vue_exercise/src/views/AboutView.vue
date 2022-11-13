156bf879a5e2e284b706348ee9110196684ec876
doodoodoong <doo_doong@hanmail.net>
2022-11-13 14:17:36 +0900

    Real_3week_homework

---
+++ 3week_homework_2/vue_exercise/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>To-do List</h1>
+  </div>
+  <div>
+    <input type="text" id="todo_input" v-model="first_todo" />
+    <button for="todo_input"></button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      todo_list: [],
+      first_todo: 'Write your todo'
+    }
+  },
+  methods: {
+    plusTodo() {}
+  }
+}
+</script>
+<style scoped></style>
